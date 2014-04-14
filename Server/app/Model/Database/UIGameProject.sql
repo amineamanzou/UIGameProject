9f198545f42dea8eca1e9be4b0342392bd91a517
Amine Amanzou <amine@lacompany.fr>
2014-04-14 11:13:49 +0200

    Adding a dump of a pre-conception of the database

---
+++ Server/app/Model/Database/UIGameProject.sql
@@ -0,0 +1,173 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Hôte: 127.0.0.1 (MySQL 5.6.10)
+# Base de données: UIGameProject
+# Temps de génération: 2014-04-14 09:10:29 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Affichage de la table badges
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `badges`;
+
+CREATE TABLE `badges` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `passivebonus` int(11) DEFAULT NULL,
+  `directbonus` int(11) DEFAULT NULL,
+  `cycleduration` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table dates
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `dates`;
+
+CREATE TABLE `dates` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table improvements
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `improvements`;
+
+CREATE TABLE `improvements` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `damage` int(11) DEFAULT NULL,
+  `critics` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `parent` varchar(255) DEFAULT NULL COMMENT 'JSON Format',
+  `date_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table mob_territoires
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mob_territoires`;
+
+CREATE TABLE `mob_territoires` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `mob_id` int(10) unsigned DEFAULT NULL,
+  `territoire_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table mobs
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mobs`;
+
+CREATE TABLE `mobs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `media` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table territoires
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `territoires`;
+
+CREATE TABLE `territoires` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `coordinate` varchar(255) DEFAULT '{0,0}' COMMENT 'JSON Format',
+  `media` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table user_badges
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_badges`;
+
+CREATE TABLE `user_badges` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `badge_id` int(10) unsigned DEFAULT NULL,
+  `date_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table user_territoires
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_territoires`;
+
+CREATE TABLE `user_territoires` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `territoire_id` int(10) unsigned DEFAULT NULL,
+  `date_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `type` enum('admin','player') NOT NULL DEFAULT 'player',
+  `experience` int(11) NOT NULL DEFAULT '0',
+  `money` int(11) NOT NULL DEFAULT '0',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `token` varchar(255) DEFAULT '',
+  `territoire_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
