d4dff74b7af66a26449fd0fa68553f32b547fe6e
Shinthujan SIVAKUMAR <shinthujan.sivakumar@gmail.com>
2014-05-25 01:19:58 +0200

    Updating menu (multiple tower choice)

--- Client/js/menu.js
+++ Client/js/menu.js
@@ -1,4 +1,17 @@
 var choix = 0;
+var tiles = "";
+
+function choixSprites() {
+	if(choix == 1) {
+		tiles = "test.png";
+	} else if(choix == 2) {
+		tiles = "monster.png";
+	} else if(choix == 3) {
+		tiles = "fairy.png";
+	} else if(choix == 4) {
+		tiles = "dog.png";
+	} 
+}
 
 function selection(e) {
 	e.style.backgroundColor='#FF8000';
@@ -11,7 +24,7 @@ function deselection(e) {
 
 function choixTour(no) {
 	choix = no;
-	
+	choixSprites();
 }
 
 //Placement des tours
@@ -19,8 +32,8 @@ window.onclick = function() {
 	if(choix != 0) {
 		var x = Math.floor(((event.clientX/TAILLE_TILE) * 480) / window.innerWidth);
 		var y = Math.floor(((event.clientY/TAILLE_TILE) * 480) / window.innerHeight);
-
-		var tour = new Tower("test.png", x, y, DIRECTION.BAS);
+		
+		var tour = new Tower(tiles, x, y, DIRECTION.BAS);
 		tourPos.push(new Array(x,y));
 		map.addTour(tour);
 	}
